bea36141f5127137174518a0869713588eb747ea
dosymep <dosymep@gmail.com>
2022-08-08 16:08:16 +0300

    added revit application

---
+++ dosymep.Revit.Engine/RevitAppInfo.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+
+using Autodesk.Revit.ApplicationServices;
+
+namespace dosymep.Revit.Engine {
+    /// <summary>
+    /// Revit application information.
+    /// </summary>
+    public class RevitAppInfo {
+        /// <summary>
+        /// Application Guid.
+        /// </summary>
+        public Guid Guid { get; set; } = new Guid("369186E7-1F68-4470-BB4F-89EB6DFF7826");
+
+        /// <summary>
+        /// Enable IFC.
+        /// </summary>
+        public bool EnableIfc { get; set; }
+        
+        /// <summary>
+        /// Api settings.
+        /// </summary>
+        public ApiSettings ApiSettings { get; set; }
+
+        /// <summary>
+        /// Autodesk licence key.
+        /// </summary>
+        public string LicenseKey { get; set; }
+
+        /// <summary>
+        /// Vendor name.
+        /// </summary>
+        public string VendorName { get; set; } = "dosymep";
+
+        /// <summary>
+        /// Application name.
+        /// </summary>
+        public string ApplicationName { get; set; } = "RevitCoreConsole";
+
+        /// <summary>
+        /// Language type.
+        /// </summary>
+        public LanguageType LanguageType { get; set; } = LanguageType.English_USA;
+    }
+
+    /// <summary>
+    /// Revit api settings.
+    /// </summary>
+    public class ApiSettings {
+        /// <summary>
+        /// Journal name.
+        /// </summary>
+        public string JournalName { get; set; }
+        
+        /// <summary>
+        /// Journal path.
+        /// </summary>
+        public string JournalPath { get; set; }
+        
+        /// <summary>
+        /// Settings file location.
+        /// </summary>
+        public string SettingsFileLocation { get; set; }
+        
+        /// <summary>
+        /// Overwrite existing files.
+        /// </summary>
+        public bool IsOverwriteExistingFiles { get; set; }
+        
+        /// <summary>
+        /// Replace existing symbols.
+        /// </summary>
+        public bool IsReplaceExistingSymbols { get; set; }
+        
+        /// <summary>
+        /// Ignore linked files on save.
+        /// </summary>
+        public bool IsIgnoreLinkedFilesOnSave { get; set; }
+        
+        /// <summary>
+        /// Force multi undo operation.
+        /// </summary>
+        public bool IsForceMultiUndoOperation { get; set; }
+        
+        /// <summary>
+        /// Update shared families. 
+        /// </summary>
+        public bool IsUpdateSharedFamilies { get; set; }
+        
+        /// <summary>
+        /// Update family parameters.
+        /// </summary>
+        public bool IsUpdateFamilyParameters { get; set; }
+        
+        /// <summary>
+        /// Ignore missing updaters.
+        /// </summary>
+        public bool IsIgnoreMissingUpdaters { get; set; }
+        
+        /// <summary>
+        /// Accept foreign files.
+        /// </summary>
+        public bool IsAcceptForeignFiles { get; set; }
+    }
+}
---
+++ dosymep.Revit.Engine/RevitApplication.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+using Autodesk.Revit;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.ApplicationServices;
+
+namespace dosymep.Revit.Engine {
+    internal class RevitApplication : IDisposable {
+        private readonly RevitAssemblyResolver _assemblyResolver;
+
+        public RevitApplication() {
+            RevitProduct = Product.GetInstalledProduct();
+            _assemblyResolver = new RevitAssemblyResolver();
+        }
+
+        public Product RevitProduct { get; }
+        public Application Application => RevitProduct.Application;
+
+        public RevitAppInfo RevitAppInfo { get; set; }
+
+        public string RevitEnginePath {
+            get => _assemblyResolver.RevitEnginePath;
+            set => _assemblyResolver.RevitEnginePath = value;
+        }
+
+        public void Open() {
+            if(string.IsNullOrEmpty(RevitEnginePath)) {
+                throw new InvalidOperationException($"{nameof(RevitEnginePath)} is not set.");
+            }
+
+            if(!Directory.Exists(RevitEnginePath)) {
+                throw new InvalidOperationException($"{nameof(RevitEnginePath)} is not found.");
+            }
+
+            if(!IsRevitPath()) {
+                throw new InvalidOperationException($"{nameof(RevitEnginePath)} is not revit path.");
+            }
+
+            _assemblyResolver.UpdateEnvironmentPaths();
+
+            RevitProduct.EnableIFC(RevitAppInfo.EnableIfc);
+            RevitProduct.SetPreferredLanguage(RevitAppInfo.LanguageType);
+
+            var appId = new ClientApplicationId(RevitAppInfo.Guid,
+                RevitAppInfo.ApplicationName, RevitAppInfo.VendorName);
+
+#if REVIT_2021_OR_LESS
+            RevitProduct.Init(appId, RevitAppInfo.LicenseKey);
+#else
+            RevitProduct.Initialize_ForAutodeskInternalUseOnly(appId, RevitAppInfo.LicenseKey);
+#endif
+        }
+
+        public void Close() {
+            RevitProduct?.Exit();
+            RevitProduct?.Dispose();
+            _assemblyResolver.Dispose();
+        }
+
+        private bool IsRevitPath() {
+            string[] fileNames = Directory.GetFiles(RevitEnginePath, "*.dll")
+                .Select(item => Path.GetFileName(item))
+                .ToArray();
+
+            return fileNames.Contains("RevitNET.dll")
+                   && fileNames.Contains("RevitAPI.dll")
+                   && fileNames.Contains("RevitAPIUI.dll");
+        }
+
+
+        #region IDisposable
+
+        public void Dispose() {
+            Close();
+        }
+
+        #endregion
+    }
+}
