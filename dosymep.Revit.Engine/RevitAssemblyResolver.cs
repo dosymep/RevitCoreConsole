a1cacc1f224a4738088300949562304d10700685
dosymep <dosymep@gmail.com>
2022-08-08 11:54:38 +0300

    added revit assembly resolver

---
+++ dosymep.Revit.Engine/RevitAssemblyResolver.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace dosymep.Revit.Engine {
+    /// <summary>
+    /// Revit assembly resolver.
+    /// </summary>
+    internal class RevitAssemblyResolver : IDisposable {
+        /// <summary>
+        /// Constructs revit assembly resolver.
+        /// </summary>
+        public RevitAssemblyResolver() {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+        }
+        
+        /// <summary>
+        /// Revit installation folder.
+        /// </summary>
+        public string RevitEnginePath { get; set; }
+
+        /// <summary>
+        /// Updates environments paths.
+        /// </summary>
+        public void UpdateEnvironmentPaths()
+        {
+            string value = string.Concat(Environment.GetEnvironmentVariable("PATH"), 
+                Path.PathSeparator, RevitEnginePath);
+            Environment.SetEnvironmentVariable("PATH", value);
+        }
+
+        private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            var assemblyName = new AssemblyName(args.Name);
+            string assemblyPath = Path.Combine(RevitEnginePath, assemblyName.Name + ".dll");
+            if (!File.Exists(assemblyPath))
+            {
+                assemblyPath = Path.Combine(RevitEnginePath, "SDA\\bin", assemblyName.Name + ".dll");
+            }
+
+            return File.Exists(assemblyPath) ? Assembly.LoadFrom(assemblyPath) : null;
+        }
+
+        #region IDisposable
+
+        public void Dispose()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve -= CurrentDomain_AssemblyResolve;
+        }
+
+        #endregion
+    }
+}
