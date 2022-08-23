a32997b434c388ffb18d28260384aa9a1a7b0aef
dosymep <dosymep@gmail.com>
2022-08-23 15:20:33 +0300

    init repository

---
+++ RevitCoreConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RevitCoreConsole {
+    internal class Program {
+        public static void Main(string[] args) {
+        }
+    }
+}
