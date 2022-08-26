faa963bfcc1f0c2c7c7cc166c6242caaba4402b3
dosymep <dosymep@gmail.com>
2022-08-26 15:07:33 +0300

    added revit exterenal application

---
+++ dosymep.Revit.Engine/RevitExternals/RevitExternalApplication.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace dosymep.Revit.Engine.RevitExternals {
+    /// <summary>
+    /// External application.
+    /// </summary>
+    internal class RevitExternalApplication : RevitExternalItem<IExternalApplication> {
+        public RevitExternalApplication(Application application)
+            : base(application) {
+        }
+
+        protected override void ExecuteExternalImpl(IExternalApplication application) {
+            UIControlledApplication controlledApplication = _application.CreateUIControlledApplication();
+            application.OnStartup(controlledApplication);
+        }
+    }
+}
