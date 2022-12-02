3d5ffe6bb042c27d1dc9a9568c53c7a0b9701602
dosymep <dosymep@gmail.com>
2022-12-02 07:14:26 +0300

    updated reference libraries

--- dosymep.Revit.Engine/CoreCommands/IForgeCommand.cs
+++ dosymep.Revit.Engine/CoreCommands/IForgeCommand.cs
@@ -1,6 +1,4 @@
-﻿using Autodesk.ExchangeStore;
-
-namespace dosymep.Revit.Engine.CoreCommands {
+﻿namespace dosymep.Revit.Engine.CoreCommands {
     public interface IForgeCommand {
         string ModelPath { get; }
         string BundlePath { get; }
